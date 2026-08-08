91947b7b59ee09f8b24b9c4577e5e6d83ca2864a
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao+github@gmail.com>
2026-08-08 15:44:16 -0300

    [fixed]: Corrige import errado.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import { fetchGitHubUser } from '$lib/services/github.service';
+import { fetchGitHubUser } from '$lib/service/github.service';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ platform }) => {
